6c52d6ed114f9e2ed0b8ce6cee8c47262fa2885f
khurshed52 <khurshedkhan52@gmail.com>
2019-04-25 14:37:40 +0400

    user added

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Users} from './user';
+import 'rxjs/add/operator/map';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
   constructor(private http: HttpClient) { }
+  //get user
   getUsers() {
     return this.http.get('http://localhost:3000/api/users');
   }
+
+
 }
