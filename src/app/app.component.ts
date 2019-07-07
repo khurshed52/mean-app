18df542e5f98cd8d46649190dd7197c1f5c17dd2
khurshed52 <khurshedkhan52@gmail.com>
2019-07-07 14:11:47 +0400

    darkmode js apply

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
+import Darkmode from 'darkmode-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mean';
   data:string= 'hello parent';
+
+  ngOnInit(){
+    var options = {
+      bottom: '64px', // default: '32px'
+      right: 'unset', // default: '32px'
+      left: '32px', // default: 'unset'
+      time: '0.5s', // default: '0.3s'
+      mixColor: '#081B33', // default: '#fff'
+      backgroundColor: '#fff',  // default: '#fff'
+      buttonColorDark: '#100f2c',  // default: '#100f2c'
+      buttonColorLight: '#fff', // default: '#fff'
+      saveInCookies: false, // default: true,
+      label: '🌓' // default: ''
+    }
+    
+    const darkmode = new Darkmode(options);
+    darkmode.showWidget();
+  }
 }
